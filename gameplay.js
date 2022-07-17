73b0eb27b43775182090b72e9552437d98d812f1
jimnta <dimitrisntarlagiannis@hotmail.com>
2022-07-17 22:32:59 +0300

    added buttons so the player can press the button to choose his option and not type it

--- gameplay.js
+++ gameplay.js
@@ -1,79 +1,55 @@
-let choices = ["paper", "rock", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('input')
 
-/* this function makes a random choice selection for the computer */
-function computerPlay(choices){
-    let computerSelection = choices[Math.floor(Math.random()*choices.length)];
-    console.log(computerSelection);
-    return computerSelection;
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-/* this function takes player's selection and also makes it case-insensitive */
-function playerPlay(){
-    let playerSelection = prompt("Choose Paper, Rock or Scissors");
-    playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
-    return(playerSelection);
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-/* this function caclulates the result of each round*/
-function playRound(computerSelection, playerSelection){
-    let result;
-    switch(true){
-        case (computerSelection === playerSelection):
-            console.log("tie");
-            result = "tie!";
-            return(result);
-        case ((computerSelection === "rock") && (playerSelection === "scissors")):
-        case ((computerSelection === "paper") && (playerSelection === "rock")):
-        case ((computerSelection === "scissors") && (playerSelection === "paper")):
-            console.log("you lost");
-            result = "you lost!";
-            return(result);
-        case ((playerSelection === "rock") && (computerSelection === "scissors")):
-        case ((playerSelection === "paper") && (computerSelection === "rock")):
-        case ((playerSelection === "scissors") && (computerSelection === "paper")):
-            console.log("you win");
-            result = "you won!";
-            return(result);
-        default:
-            console.log("wrong input");
-            result = "wrong input!";
-            return(result);
-    }
-}
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
-/* this function create a game of 5 rounds and announce the winner at the end, if the user 
-input is not valid it resets the round */ 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        let computerSelection = computerPlay(choices);
-        let playerSelection = playerPlay();
-        let result = playRound(computerSelection, playerSelection);
-        if (result === "you lost!"){
-            computerScore += 1;
-            console.log(computerScore);
-            console.log(playerScore);
-        } else if (result === "you won!"){
-            playerScore += 1;
-            console.log(computerScore);
-            console.log(playerScore);
-        } else if (result === "wrong input!"){
-            console.log(computerScore);
-            console.log(playerScore);
-            i = i - 1;
-        } else {
-            console.log(computerScore);
-            console.log(playerScore);
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
         }
     }
-    if (computerScore >= playerScore){
-        console.log("game over, you lost");
-    } else if (computerScore <= playerScore){
-        console.log("game over, you won");
-    } else {
-        console.log("game over, it's a tie");
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
 }
-game();
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
